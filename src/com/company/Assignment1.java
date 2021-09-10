813fb59988f2a964b098de3486171e087634fa79
Inta Slapna <Inta.slapnja@inbox.lv>
2021-09-10 21:25:59 +0300

    Initial commit

---
+++ src/com/company/Assignment1.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Assignment1 {
+
+    public static void main(String[] args) {
+        short numberOne = 4;
+        short numberTwo = 10;
+        if (numberOne == numberTwo) {
+            System.out.println("Equal");
+        } else if (numberOne > numberTwo) {
+            System.out.println("Number One bigger");
+        } else {
+            System.out.println("Number Two bigger");
+        }
+
+        numberOne = 10;
+        numberTwo = 5;
+
+    }
+}
---
+++ src/com/company/Assignment2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Assignment2 {
+    public static void main(String[] args) {
+        short numberOne = 4;
+        short numberTwo = 7;
+        System.out.println("NumberOne is " + (numberOne % 2 != 0?"odd":"even"));
+        System.out.println("NumberTwo is " + (numberTwo % 2 != 0?"odd":"even"));
+        System.out.println("NumberOne is " + (numberOne % 2 != 0?"odd":"even"));
+    }
+    }
